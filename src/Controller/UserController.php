ae3515c73979f92a6f4127b44337d21e741fd4db
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2022-10-13 16:22:19 +0200

    Try to add translation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class UserController extends AbstractController
 {
     #[Route('/profile', 'app_profile')] 
-    public function profile() {
+    public function profile() : Response
+    {
         return $this->render('user/profile.html.twig');
+    }
+
+    #[Route('/change-locale/{locale}', 'app_change_locale', ['locale' => 'en|fr_FR'])] 
+    public function changeLocale(Request $request, string $locale) : Response
+    {
+        if ($request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $locale));
+        }
+    
+        return $this->redirect($request->headers->get('referer'));
     }
 }
